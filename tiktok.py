0dcec2b78a205b655146575c10880e5c0e00e289
josephinehemingway <josephine.hemingway@gmail.com>
2022-09-20 18:56:22 +0800

    weee

--- tiktok.py
+++ tiktok.py
@@ -3,30 +3,25 @@
 # playwright install 
 # pip install git+https://github.com/Daan-Grashoff/TikTok-Api
 
-
 from TikTokApi import TikTokApi
 import pandas as pd
-#import prettify
-
  
 api = TikTokApi()
 vids = api.hashtag(name="Switzerland").videos()
 
-#vids is an iterator
-# print(type(vids))
-
 # iterate through iterator
 for trending_video in vids:
     print("----------")
-    # COMMENTS
-    # print(trending_video.comments())
-    # for comment in trending_video.comments():
-    #     print(comment)
 
     # DOWNLOAD ADDRESS
     info = trending_video.info()
     print(info['video']['downloadAddr'])
 
+    # COMMENTS
+    # print(trending_video.comments())
+    # for comment in trending_video.comments():
+    #     print(comment)
+    
     # stats = trending_video.stats
     # author = trending_video.author.username
 
