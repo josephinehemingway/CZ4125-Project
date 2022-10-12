1bfe2405432ee43a57ba2c421ada3ab9e347dd7f
Joey Lim <55871896+sylime@users.noreply.github.com>
2022-10-12 16:02:25 +0800

    Google itinerary function, still in progress

--- tiktok.py
+++ tiktok.py
@@ -10,7 +10,8 @@ import pandas as pd
 
  
 api = TikTokApi()
-vids = api.hashtag(name="Switzerland").videos()
+vids = api.hashtag(name="Switzerland").videos(count=100)
+count=0
 
 #vids is an iterator
 # print(type(vids))
@@ -18,16 +19,34 @@ vids = api.hashtag(name="Switzerland").videos()
 # iterate through iterator
 for trending_video in vids:
     print("----------")
+<<<<<<< HEAD
     # COMMENTS
     # print(trending_video.comments())
     for comment in trending_video.comments():
          print(comment)
 
+=======
+    
+>>>>>>> a8cae99 (Google itinerary function, still in progress)
     # DOWNLOAD ADDRESS
-    info = trending_video.info()
-    print(info['video']['downloadAddr'])
+    #info = trending_video.info()
+    #print(info['video']['downloadAddr'])
 
+<<<<<<< HEAD
+=======
+    # COMMENTS
+    #print(trending_video.comments())
+    #for comment in trending_video.comments():
+        #print(comment)
+    
+>>>>>>> a8cae99 (Google itinerary function, still in progress)
     # stats = trending_video.stats
-    # author = trending_video.author.username
+    author = trending_video.author.username
+    print(author)
+    count = count +1
+
+
+print(count)
+
 
 
