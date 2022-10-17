bf23df992528e20fb3a59a98da2e54820a5e7d6a
josephinehemingway <josephine.hemingway@gmail.com>
2022-10-17 22:25:11 +0800

    formatted code and edited search bar css >:(

--- travelui/src/App.tsx
+++ travelui/src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import 'antd/dist/antd.min.css';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import "antd/dist/antd.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Explore from "./pages/Explore/Explore";
 import Footer from "./components/reusable/Footer";
 
 function App() {
-  return (
-    <Router>
-      {/*<Navbar/>*/}
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/explore/:country/:destination' element={<Explore />} />
-        {/*<Route path='/itinerary/:destination' element={<Itinerary/>} />*/}
-      </Routes>
-      <Footer />
-    </Router>
-  );
+    return (
+        <Router>
+            {/*<Navbar/>*/}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route
+                    path="/explore/:country/:destination"
+                    element={<Explore />}
+                />
+                {/*<Route path='/itinerary/:destination' element={<Itinerary/>} />*/}
+            </Routes>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
--- travelui/src/pages/Explore/Explore.tsx
+++ travelui/src/pages/Explore/Explore.tsx
@@ -1,56 +1,63 @@
 import React from "react";
 import "../Pages.css";
-import {Container, StyledPageTitle} from "../../components/reusable/Styles";
-import {Link, useLocation} from "react-router-dom";
+import { Container, StyledPageTitle } from "../../components/reusable/Styles";
+import { Link, useLocation } from "react-router-dom";
 import ExploreBanner from "../../components/Explore/ExploreBanner";
 import London from "../../assets/images/London.jpeg";
-import {Breadcrumb, Tabs} from "antd";
+import { Breadcrumb, Tabs } from "antd";
 // import TikTokSection from "../../components/reusable/TikTok/TikTokSection";
 
-const Explore: React.FC = ({}) => {
-  const countryName = useLocation().pathname.split("/")[2];
-  const destinationName = useLocation().pathname.split("/")[3];
+const Explore: React.FC = () => {
+    const countryName = useLocation().pathname.split("/")[2];
+    const destinationName = useLocation().pathname.split("/")[3];
 
-  return (
-    <body className="home">
-      <ExploreBanner coverUrl={London} destinationName={destinationName} />
-      <Container
-        width="70%"
-        height={"155vh"}
-        paddingtop={"2rem"}
-        align="flex-start"
-      >
-        <Breadcrumb style={{ fontSize: "20px" }}>
-          <Breadcrumb.Item>
-            <Link to="/">Home</Link>
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>
-            <Link to="/">Explore</Link>
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>{destinationName}</Breadcrumb.Item>
-        </Breadcrumb>
-        <StyledPageTitle> {`${destinationName}, ${countryName}`} </StyledPageTitle>
-        <Tabs defaultActiveKey="1"
-              size="large"
-              tabBarGutter={50}
-              tabBarStyle={{fontFamily: 'Poppins-Medium'}}
-        >
-          <Tabs.TabPane tab="Attractions" key="1" >
-            Attractions Tab
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="Food" key="2">
-            Food Tab
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="Accommodations" key="3">
-            Accommodations Tab
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="Guides" key="4">
-            Guides Tab
-          </Tabs.TabPane>
-        </Tabs>
-      </Container>
-    </body>
-  );
+    return (
+        <body className="home">
+            <ExploreBanner
+                coverUrl={London}
+                destinationName={destinationName}
+            />
+            <Container
+                width="70%"
+                height={"155vh"}
+                paddingtop={"2rem"}
+                align="flex-start"
+            >
+                <Breadcrumb style={{ fontSize: "20px" }}>
+                    <Breadcrumb.Item>
+                        <Link to="/">Home</Link>
+                    </Breadcrumb.Item>
+                    <Breadcrumb.Item>
+                        <Link to="/">Explore</Link>
+                    </Breadcrumb.Item>
+                    <Breadcrumb.Item>{destinationName}</Breadcrumb.Item>
+                </Breadcrumb>
+                <StyledPageTitle>
+                    {" "}
+                    {`${destinationName}, ${countryName}`}{" "}
+                </StyledPageTitle>
+                <Tabs
+                    defaultActiveKey="1"
+                    size="large"
+                    tabBarGutter={50}
+                    tabBarStyle={{ fontFamily: "Poppins-Medium" }}
+                >
+                    <Tabs.TabPane tab="Attractions" key="1">
+                        Attractions Tab
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab="Food" key="2">
+                        Food Tab
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab="Accommodations" key="3">
+                        Accommodations Tab
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab="Guides" key="4">
+                        Guides Tab
+                    </Tabs.TabPane>
+                </Tabs>
+            </Container>
+        </body>
+    );
 };
 
 export default Explore;
