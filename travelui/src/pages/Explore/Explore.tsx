0209fd8c47ccac84de8a8b1027d53b77c9596bf1
josephinehemingway <josephine.hemingway@gmail.com>
2022-11-10 13:20:48 +0800

    refactored tabs

--- travelui/src/pages/Explore/Explore.tsx
+++ travelui/src/pages/Explore/Explore.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../Pages.css";
-import { Container, StyledPageTitle } from "../../components/reusable/Styles";
+import { Container, StyledPageTitle, StyledTabs } from "../../components/reusable/Styles";
 import { Link, useLocation } from "react-router-dom";
 import ExploreBanner from "../../components/Explore/ExploreBanner";
 import London from "../../assets/images/London.jpeg";
-import { Breadcrumb, Tabs } from "antd";
+import { Breadcrumb } from "antd";
 // import TikTokSection from "../../components/reusable/TikTok/TikTokSection";
+
+const {TabPane} = StyledTabs
 
 const Explore: React.FC = () => {
     const countryName = useLocation().pathname.split("/")[2];
@@ -36,34 +38,26 @@ const Explore: React.FC = () => {
                     {" "}
                     {`${destinationName}, ${countryName}`}{" "}
                 </StyledPageTitle>
-                <Tabs
+
+                <StyledTabs
+                    size={'large'}
                     defaultActiveKey="1"
-                    size="large"
                     tabBarGutter={50}
-                    tabBarStyle={{ fontFamily: "Poppins-Medium" }}
-                    items={[
-                        {
-                            label: `Attractions`,
-                            key: '1',
-                            children: ` Attractions Tab`,
-                        },
-                        {
-                            label: `Food`,
-                            key: '2',
-                            children: `Food Tab`,
-                        },
-                        {
-                            label: `Accommodations`,
-                            key: '3',
-                            children: `Accommodations Tab`,
-                        },
-                        {
-                            label: `Guides`,
-                            key: '4',
-                            children: `Guides Tab`,
-                        },
-                    ]}
-                />
+                    tabBarStyle={{fontFamily: 'Poppins-Medium'}}
+                >
+                    <TabPane tab="Attractions" key="1">
+                         dump attractions here
+                    </TabPane>
+                    <TabPane tab="Food" key="2">
+                        dump food here
+                    </TabPane>
+                    <TabPane tab="Accommodations" key="3">
+                        dump accoms here
+                    </TabPane>
+                    <TabPane tab="Guides" key="4">
+                        dump guides here
+                    </TabPane>
+                </StyledTabs>
             </Container>
         </body>
     );
