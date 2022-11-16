f4cc2c6b8cfaa8c491c2bc59353e901ac2941b10
josephinehemingway <josephine.hemingway@gmail.com>
2022-11-16 17:54:14 +0800

    added search function for home page explore search bar

--- travelui/src/pages/Itinerary/Itinerary.tsx
+++ travelui/src/pages/Itinerary/Itinerary.tsx
@@ -7,15 +7,7 @@ import {TIKTOK_LIST} from "../../utils/const";
 import ItineraryBanner from "../../components/Itinerary/ItineraryBanner";
 import TikTokSection from "../../components/reusable/TikTok/TikTokSection";
 import Attractions from "../../components/Explore/Attractions";
-
-function capitalise(destinationName: string) {
-    const arr = destinationName.split(" ");
-    for (var i = 0; i < arr.length; i++) {
-        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
-
-    }
-    return arr.join(" ");
-}
+import {capitalise} from "../../utils/helperfunctions"
 
 const Itinerary: React.FC = () => {
     const destinationDetails = useLocation().pathname.split("/")[2];
