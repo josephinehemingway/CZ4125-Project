4857ed8260b089bba97c66c7b5ff9ffa2351e0c5
josephinehemingway <josephine.hemingway@gmail.com>
2022-11-16 15:39:51 +0800

    added loading for accoms

--- travelui/src/components/reusable/Cards/AccomsCard.tsx
+++ travelui/src/components/reusable/Cards/AccomsCard.tsx
@@ -15,7 +15,7 @@ const AccomsCard: React.FC<Props> = ({url, name, rating, onClick}) => {
     return (
         <div className='acc-card' onClick={onClick}>
             <img className='attr-img' src={url} height="100%"  alt=''/>
-            <div className={lengthName > 20 ? 'acc-desc-2lines' : 'acc-desc'}>
+            <div className={lengthName > 21 ? 'acc-desc-2lines' : 'acc-desc'}>
                 <h2 className="acc-name"> {name} </h2>
                 <div style={{flexDirection: 'row', display: 'flex', alignItems: 'center', justifyContent: 'flex-start'}}>
                     <h2 className="acc-rating"> {rating} </h2>
