f04b176f983c649b75ffda125b6f20a0771170b8
shannon <skwc18400@gmail.com>
2022-11-25 01:44:57 +0800

    display only years 2015 and more

--- travelui/flask-server/api_functions.py
+++ travelui/flask-server/api_functions.py
@@ -376,7 +376,8 @@ def get_passengers(city):
     country = place.split(' ').pop()
     df = pd.read_csv('./number-of-air-transport-passengers-carried.csv')
     df['Passengers'] = df['Passengers'].astype(int)
-    df = df.loc[df['Entity'] == country].reset_index(drop=True)
+    df = df.loc[(df['Entity'] == country) & (
+        df['Year'] > 2014)].reset_index(drop=True)
     passenger_dict = {}
     passenger_dict['City'] = city
     passenger_dict['Country'] = country
